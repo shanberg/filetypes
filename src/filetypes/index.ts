c6d9fd66c0ce32550c4a0e29632e53fcd295fccd
Stuart Hanberg <shanberg@users.noreply.github.com>
2024-06-22 13:58:14 -0400

    remove redundant description

--- src/filetypes/index.ts
+++ src/filetypes/index.ts
@@ -1,43 +1,5 @@
 /// <reference path="../types.d.ts" />
-// Mappings between file extension,
-// mimetype, and human-readable file 'kind'.
 
-// Kind indicates the type of file
-// as it exists to the user, rather 
-// than as it exists to the computer.
-// Both are useful, but kind is
-// preferred in casual, intuitive settings
-// where the user is concerned with
-// the content of a system, not the
-// system itself.
-
-// Kinds should be singular.
-// Kinds are lower-case, except where they
-// refer to proper nouns or are abbreviated
-// in the file extension, e.g. "HyperText
-// Markup Language"
-// Avoid using "file" at the end of the Kind.
-
-
-
-// Official resource of Media Types (mimetypes)
-// https://www.iana.org/assignments/media-types/media-types.xhtml
-
-// Database of all mimetypes
-// https://github.com/jshttp/mime-db
-
-
-
-
-// {
-//   "ext": ["bmp"],
-//   "kind": ["Bitmap Image"],
-//   "mimetype": ["image/bmp"]
-//   "documentType"?: "spreadsheet"
-// },
-
-
-// The FileType arrays are separated for convenience.
 import archive from "./archive.ts";
 import audio from "./audio.ts";
 import binary from "./binary.ts";
@@ -55,7 +17,6 @@ import source from "./source.ts";
 import text from "./text.ts";
 import uncategorized from "./uncategorized.ts";
 import video from "./video.ts";
-
 
 const combined: Filetype[] = [
   ...archive,
